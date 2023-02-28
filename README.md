9e56ca11d954c2adef1285feca473b3913020f06
Standard <oanlara8@gmail.com>
2023-02-28 15:15:28 -0700

    creating models

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ Django REST framework
 Docker
 Go
 
+This is the database schema:
+
+![database schema](https://i.imgur.com/9gV6sQ6.png)
+
+First name, last name, additional user information is intentionally left out for this demo.
+
 https://mui.com/material-ui/getting-started/installation/
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=50, unique=True)
+    hashed_password = models.CharField(max_length=500)
+
+class Account(models.Model):
+    id = models.AutoField(primary_key=True)
+    account_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=16, unique=True)
+    current_balance = models.FloatField()
+
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    transaction_type = models.CharField(max_length=50)
+    note = models.CharField(max_length=500)
+    amount = models.FloatField()
