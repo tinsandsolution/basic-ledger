1324f80115877153b5781fa881c542f471312f0f
Standard <oanlara8@gmail.com>
2023-02-28 15:41:20 -0700

    seed data

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'core'
 ]
 
