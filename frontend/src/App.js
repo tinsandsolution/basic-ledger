4bf73cc7670b82ef7ad76a4a8d18e1c9e00f9aaf
Standard <oanlara8@gmail.com>
2023-03-02 17:08:17 -0700

    skeleton loggedoutpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
-import LoginForm from './auth/login';
+import LoggedOutPage from './components/LoggedOutPage';
 import { useSelector, useDispatch } from 'react-redux';
+import { authenticate } from './store/session';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
   const user = useSelector(state => state.session.user);
 
+  useEffect(() => {
+    (async() => {
+      await dispatch(authenticate());
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
+  if(!loaded) {
+    return null;
+  }
   return (
     <div className="App">
       {/* if user is null, show loginform */}
       <div>hi</div>
-      {user ? <div>hi</div> : <LoginForm />}
+      {user ? <div>hi</div> : <LoggedOutPage />}
     </div>
   );
 }
---
+++ frontend/src/components/LoggedOutPage/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { redirect } from 'react-router-dom';
+// import { login } from '../../store/session';
+import LoginForm from '../auth/login';
+
+const LoggedOutPage = () => {
+
+
+  return (
+    <div className='logged-out'>
+        <LoginForm />
+    </div>
+  );
+};
+
+export default LoggedOutPage;
