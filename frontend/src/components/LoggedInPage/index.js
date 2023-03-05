b0555ccfdf4e5204abe700c86d23d6c18dd7d2c5
Standard <oanlara8@gmail.com>
2023-03-05 15:47:09 -0700

    sidebar for logged in view

--- frontend/src/components/LoggedInPage/index.js
+++ frontend/src/components/LoggedInPage/index.js
@@ -3,16 +3,28 @@ import { useSelector, useDispatch } from 'react-redux';
 import { redirect } from 'react-router-dom';
 // import { login } from '../../store/session';
 import './LoggedInPage.css';
+import { Button } from '@mui/material';
 
 const LoggedInPage = () => {
-  const [loggedIn, setLoggedIn] = useState("login");
+  const [view, setView] = useState("accounts");
   const user = useSelector(state => state.session.user);
 
   return (
     <div className='logged-in'>
       <div className='sidebar'>
         <div className='sidebar__header'> Hi, {user}! </div>
-        <div className='sidebar__content'> </div>
+        <div className='sidebar__content'>
+          <Button
+            variant={view === "accounts" ? "contained" : "outlined"}
+            sx={{ width: '80%',}}
+            onClick={() => setView("accounts")}
+          >Accounts</Button>
+          <Button
+            variant={view === "transactions" ? "contained" : "outlined"}
+            sx={{ width: '80%',}}
+            onClick={() => setView("transactions")}
+          >Transactions</Button>
+        </div>
       </div>
       <div className='main'></div>
     </div>
